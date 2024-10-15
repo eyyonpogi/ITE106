3abd66996a1f0b730c246083fc2d47158e3969b8
Aeron Jay Pesebre <aeronjaypesebre@gmail.com>
2024-10-15 15:03:39 +0800

    Update Main.java

--- 02TextFileManipulationScannerLoop/Main.java
+++ 02TextFileManipulationScannerLoop/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class main{
+public class Main{
 
     public static void main(String[] args) {
         try {
